2f2269616fb34a14a663f3cec659fc9e66790988
FrancisNgugi05 <francis.ngugi@student.moringaschool.com>
2023-12-14 19:39:30 +0300

    alx

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = "bob";
+var bestCustomer;
+const LeastFavoriteCustomer = "could be bob";
+function upperCaseCustomerName () {
+    customerName = customerName.toUpperCase();
+    console.log (customerName);
+}
+function setBestCustomer () {
+    return bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer () {
+    return bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer () {
+    return LeastFavoriteCustomer = "maybe bob"
+}
